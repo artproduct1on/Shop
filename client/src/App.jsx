c4788dafbe54be526214c4c5a4ee81f9148b5ae8
Artem <artem.99.zaikin@gmail.com>
2025-05-12 22:26:08 +0200

    add File structure, routing, all dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const Products = lazy(() => import("./pages/Products"));
+const Product = lazy(() => import("./pages/Product"));
+const Categories = lazy(() => import("./pages/Categories"));
+const Cart = lazy(() => import("./pages/Cart"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+
+function App() {
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default App
